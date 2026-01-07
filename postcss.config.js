f392dbaf2c187451e367d1a9c2f6dde7238198f2
Aagnesh shifak <167229723+Aagneshshifak@users.noreply.github.com>
2026-01-07 10:04:29 +0530

    api bug fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-module.exports = () => {
-  const plugins = [];
-  try {
-    // include tailwind if present
-    require.resolve('tailwindcss');
-    plugins.push(require('tailwindcss'));
-  } catch {}
-  try {
-    plugins.push(require('autoprefixer'));
-  } catch {}
-  return { plugins };
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
